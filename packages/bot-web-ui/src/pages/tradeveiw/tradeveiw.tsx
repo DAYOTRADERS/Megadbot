192af85541e5f6834b7b7133d10761a6a5fc21ff
Arnoldekima <arnoldekima@gmail.com>
2025-10-19 09:16:12 +0300

    mumo

--- packages/bot-web-ui/src/pages/tradeveiw/tradeveiw.tsx
+++ packages/bot-web-ui/src/pages/tradeveiw/tradeveiw.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './tradeveiw.module.scss'; // assuming you renamed it correctly
-import Tradeveiw from '../tradeveiw';
 
 const Tradeveiw = () => {
     return (
         <div className={styles.container}>
             <iframe
                 src="https://charts.deriv.com/deriv"
-                title="DollarPrinters Analysis"
+                title="Tradeveiw"
                 className={styles.iframe}
                 allowFullScreen
             />
