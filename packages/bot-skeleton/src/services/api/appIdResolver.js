374a0c322272ec45d4e0fc3d23fab707a0937449
Arnoldekima <arnoldekima@gmail.com>
2025-10-23 15:34:58 +0300

    mumo

--- packages/bot-skeleton/src/services/api/appIdResolver.js
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -1,14 +1,9 @@
 const AppIds = Object.freeze({
-    'deriv.app': '106019
-', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
-    'staging.deriv.app': '106019
-',
-    'app.deriv.com': '106019
-',
-    'staging-app.deriv.com': '106019
-',
-    'goon-puce.vercel.app': '106019
-',
+    'deriv.app': '106019', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '106019',
+    'app.deriv.com': '106019',
+    'staging-app.deriv.com': '106019',
+    'goon-puce.vercel.app': '106019',
 });
 
 export default AppIds;
